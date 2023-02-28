0c25872a4eb1bedf4048aee57035ce0d960b4e5a
Luke Arntz <luke.arntz@swimlane.com>
2023-02-28 09:28:39 -0500

    using flags to create subcommands
    Short example of create two subcommands with flagsets.

---
+++ flags_subcommand.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	ew := flag.NewFlagSet("nw", flag.ExitOnError)
+	ewInterstate := ew.Int("interstate", 0, "Which interstate we should use.")
+
+	ns := flag.NewFlagSet("ns", flag.ExitOnError)
+	nsInterstate := ns.Int("interstate", 0, "Which interstate we should use.")
+
+	if len(os.Args) == 1 {
+		fmt.Println("You must enter a subcommand")
+		fmt.Println(" ns for north/south")
+		fmt.Println(" ew for east/west")
+		return
+	}
+
+	switch os.Args[1] {
+	case "ew":
+		ew.Parse(os.Args[2:])
+		fmt.Println(*ewInterstate)
+	case "ns":
+		ns.Parse(os.Args[2:])
+		fmt.Println(*nsInterstate)
+	default:
+		fmt.Printf("%q is not a valid subcommand\n", os.Args[1])
+		os.Exit(1)
+
+	}
+}
